546276d0c1de08fac855b9ec2029ae8c2ca862ae
刘杰文 <1635370493@qq.com>
2022-11-30 00:31:22 +0800

    Version 1 complete

--- autocheck.bat
+++ autocheck.bat
@@ -1,6 +1,10 @@
 @echo off
 echo L4D2 Addons Offline Mode Assistant for CN Players(Developed by TTPeter)
 echo Version: 1.0
+echo Date: 2022/11/30
+echo Developers: TTPeter/LiuJiewenTT;
+echo License: MIT License
+echo Github Project Link: https://github.com/LiuJiewenTT/L4D2_OfflineAssistant
 echo -----------------------------------------------------------------------
 
 rem Settings
@@ -42,7 +46,7 @@ set flag=false
         ) else (
             robocopy /S /MT:32 /NP "%profiledir%" "%workshopdir%"
         )
-        echo Copy Finish
+        echo Copy Complete.
         echo mark>>"%markpath%"
         echo Finish.
     )
